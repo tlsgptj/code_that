04f1c4578469cbf8a67c322bc8591d7a918b7ad4
신혜서 <125953570+tlsgptj@users.noreply.github.com>
2024-12-29 12:26:32 +0900

    소스 간단히

--- src/index.js
+++ src/index.js
@@ -1,17 +1,11 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import ReactDOM from "react-dom";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+ReactDOM.render(
+  <>
+    <h1 id="title">가위바위보</h1>
+    <button className="hand">가위</button>
+    <button className="hand">바위</button>
+    <button className="hand">보</button>
+  </>
+  , document.getElementById("root")
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
