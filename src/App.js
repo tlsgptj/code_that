4b3e1612d86748fda2e7c6c89154777987a613a2
신혜서 <125953570+tlsgptj@users.noreply.github.com>
2024-12-29 13:51:05 +0900

    state 설정

--- src/App.js
+++ src/App.js
@@ -1,9 +1,30 @@
+import { useState } from 'react';
 import Dice from './Dice';
+import Button from './Button';
+
+function random(n) {
+  return Math.ceil(Math.random() * n);
+}
 
 function App() {
-  return <div>
-    <Dice color="blue"/>
-    </div>;
+  const [number, setNumber] = useState(1);
+
+  const handleRollClick = () => {
+    const nextNum = random(6);
+    setNumber(nextNum);
+  };
+
+  const handleClearClick = () => {
+    setNumber(1);
+  }
+
+  return (
+    <div>
+      <Button onClick={handleRollClick}>던지기</Button>
+      <Button onClick={handleClearClick}>처음부터</Button>
+      <Dice color="red" num={number} />
+    </div>
+  );
 }
 
 export default App;
