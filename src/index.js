a6bb26b1fa83160dd9bd5039ada4712773f86c8d
신혜서 <125953570+tlsgptj@users.noreply.github.com>
2024-12-29 12:39:52 +0900

    이미지 빌드

--- src/index.js
+++ src/index.js
@@ -1,11 +1,17 @@
 import ReactDOM from "react-dom";
 
+
+function handleClick() {
+  console.log('가위바위보!');
+}
+
 ReactDOM.render(
   <>
     <h1 id="title">가위바위보</h1>
-    <button className="hand">가위</button>
-    <button className="hand">바위</button>
-    <button className="hand">보</button>
+    <h2>{getResult(me, other)}</h2>
+    <button onClick={handleClick}>가위</button>
+    <button onClick={handleClick}>바위</button>
+    <button onClick={handleClick}>보</button>
   </>
   , document.getElementById("root")
 );
